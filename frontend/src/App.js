19fc683db926c8eb9b7813679494614222ea79ad
Rahim Iqbal <superiqu@gmail.com>
2021-03-10 00:31:20 +0600

    add react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,23 @@
+import React from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Container } from 'react-bootstrap'
+import HomeScreen from './screens/HomeScreen'
+import ProductSceen from './screens/ProductSceen'
+
 const App = () => {
   return (
-    <>
-      <h1>hello</h1>
-    </>
+    <Router>
+      <Header/>
+      <main className="py-3">
+        <Container>
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductSceen}/>
+        </Container>
+      </main>
+      <Footer/>
+    </Router>
   );
 }
 
