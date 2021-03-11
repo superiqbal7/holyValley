8897e82bf800b369588b5ca286aa02b3297975f4
Rahim Iqbal <superiqu@gmail.com>
2021-03-12 01:23:46 +0600

    add productlist reducer

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,5 +1,14 @@
-export const productReducer = (state = { products: [] }, action) => {
+import * as productConstants from '../constants/productConstants.js'
+
+export const productListReducer = (state = { products: [] }, action) => {
   switch (action.type) {
-    case 'PRODUCT '
+    case productConstants.PRODUCT_LIST_REQUEST:
+      return { loading: true, products: [] }
+    case productConstants.PRODUCT_LIST_SUCCESS:
+      return { loading: false, products: action.payload }
+    case productConstants.PRODUCT_LIST_FAIL:
+      return { loading: false, error: action.payload }
+    default:
+      return state
   }
 }
