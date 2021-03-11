7ed00a1bb5048b079b261d7f0be8242620c368ae
Rahim Iqbal <superiqu@gmail.com>
2021-03-12 01:23:12 +0600

    add product action

---
+++ frontend/src/actions/productAction.js
@@ -0,0 +1,27 @@
+import axios from 'axios'
+import * as productConstants from '../constants/productConstants.js'
+
+export const listProduct = () => async (dispatch) => {
+  try {
+    dispatch(
+      {
+        type: productConstants.PRODUCT_LIST_REQUEST
+      }
+    )
+    const { data } = await axios.get('/api/products')
+
+    dispatch(
+      {
+        type: productConstants.PRODUCT_LIST_SUCCESS,
+        payload: data
+      }
+    )
+  } catch (error) {
+      dispatch(
+        {
+          type: productConstants.PRODUCT_LIST_FAIL,
+          payload: error.response && error.response.data.message ? error.response.data.message : error.message
+        }
+      )
+  }
+}
