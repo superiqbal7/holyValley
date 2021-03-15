54d199c22e3d6b73d8c0f25ccf60cf608df64604
Rahim Iqbal <superiqu@gmail.com>
2021-03-15 18:51:19 +0600

    add product details action

--- frontend/src/actions/productAction.js
+++ frontend/src/actions/productAction.js
@@ -25,3 +25,28 @@ export const listProduct = () => async (dispatch) => {
       )
   }
 }
+
+export const listProductDetails = (id) => async (dispatch) => {
+  try {
+    dispatch(
+      {
+        type: productConstants.PRODUCT_DETAILS_REQUEST
+      }
+    )
+    const { data } = await axios.get(`/api/products/${id}`)
+
+    dispatch(
+      { 
+        type: productConstants.PRODUCT_DETAILS_SUCCESS,
+        payload: data
+      }
+    )
+  } catch (error) {
+    dispatch(
+      {
+        type: productConstants.PRODUCT_DETAILS_FAIL,
+        payload: error.response && error.response.data.message ? error.response.data.message : error.message
+      }
+    )
+  }
+}
