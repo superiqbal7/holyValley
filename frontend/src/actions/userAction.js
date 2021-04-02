98edfe406589b1c6bfbd3b5c7e63f0f4557daa01
Rahim Iqbal <superiqu@gmail.com>
2021-04-02 15:31:34 +0600

    add register action

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -39,3 +39,41 @@ export const logout = () => (dispatch) => {
     type: userConstants.USER_LOGOUT
   })
 }
+
+export const register = (name, email, password) => async (dispatch) => {
+  try {
+    dispatch({
+      type: userConstants.USER_REGISTER_REQUEST
+    })
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    const { data } = await axios.post(`/api/users`, { name, email, password }, config)
+
+    dispatch({
+      type: userConstants.USER_REGISTER_SUCCESS,
+      payload: data
+    })
+
+    dispatch({
+      type: userConstants.USER_LOGIN_SUCCESS,
+      payload: data
+    })
+
+    localStorage.setItem('userInfo', JSON.stringify(data))
+
+  } catch (error) {
+    console.log(error);
+    dispatch(
+      {
+        type: userConstants.USER_REGISTER_FAIL,
+        payload: error.response && error.response.data.message ? error.response.data.message : error.message
+      }
+    )
+  }
+}
+
