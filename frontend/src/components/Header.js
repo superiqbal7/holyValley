c42d2da0efe47f7faf2cd0620f76e30ae1af4bd7
Rahim Iqbal <superiqu@gmail.com>
2021-04-02 12:57:56 +0600

    add user dropdown

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { LinkContainer } from 'react-router-bootstrap'
-import {Container, Navbar, Nav } from 'react-bootstrap'
+import {Container, Navbar, Nav, NavDropdown } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import { logout } from '../actions/userAction'
 
 const Header = () => {
+  const dispatch = useDispatch()
+
+  const userLogin = useSelector(state => state.userLogin)
+  const { userInfo } = userLogin
+
+  const logoutHandler = () => {
+    dispatch(logout())
+  }
+
   return (
     <header>
       <Navbar bg="primary" variant='dark' expand="lg" collapseOnSelect>
@@ -21,12 +32,23 @@ const Header = () => {
                   Cart
                 </Nav.Link>
               </LinkContainer>
-              
-              <LinkContainer to='/login'>
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id='username'>
+                  <LinkContainer to='/profile'>
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : 
+                <LinkContainer to = '/login'>
                 <Nav.Link>
-                  <i className="fas fa-user"></i>
-                Sign In</Nav.Link>
+              <i className="fas fa-user"></i>
+              Sign In</Nav.Link>
               </LinkContainer>
+              }
+
               
             </Nav>
           </Navbar.Collapse>
