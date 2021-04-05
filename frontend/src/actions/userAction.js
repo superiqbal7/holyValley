a9b8ce0f1a598889961fea4258e3a00704703a25
Rahim Iqbal <superiqu@gmail.com>
2021-04-05 20:08:12 +0600

    add getUserDetails action

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -77,3 +77,36 @@ export const register = (name, email, password) => async (dispatch) => {
   }
 }
 
+export const getUserDetails = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: userConstants.USER_DETAILS_REQUEST
+    })
+
+    const { userLogin: { userInfo } } = getState()
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${userInfo.token}`
+      }
+    }
+
+    const { data } = await axios.get(`/api/users/${id}`, config)
+
+    dispatch({
+      type: userConstants.USER_DETAILS_SUCCESS,
+      payload: data
+    })
+
+  } catch (error) {
+    console.log(error);
+    dispatch(
+      {
+        type: userConstants.USER_DETAILS_FAIL,
+        payload: error.response && error.response.data.message ? error.response.data.message : error.message
+      }
+    )
+  }
+}
+
