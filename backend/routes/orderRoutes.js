edb259c70e096b9dd81208ad9ca1e5a42ac29aa6
Rahim Iqbal <superiqu@gmail.com>
2021-04-08 01:10:22 +0600

    add order routes

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import {
+  addOrderItems
+} from '../controller/orderController.js'
+import { protect } from '../middleware/authMiddleware.js'
+
+const router = express.Router()
+
+router.route('/').post(protect, addOrderItems)
+
+
+
+
+export default router
