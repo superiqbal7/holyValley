9517889a89f0ce2514ca7e91755d05b7cd3fb9e3
Rahim Iqbal <superiqu@gmail.com>
2021-04-09 01:35:43 +0600

    add payorder order details action

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -33,3 +33,77 @@ export const createOrder = (order) => async (dispatch, getState) => {
     )
   }
 }
+
+export const getOrderDetails = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: orderConstants.ORDER_DETAILS_REQUEST
+    })
+
+    const { userLogin: { userInfo } } = getState()
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`
+      }
+    }
+
+    const { data } = await axios.get(`/api/orders/${id}`, config)
+
+    dispatch({
+      type: orderConstants.ORDER_DETAILS_SUCCESS,
+      payload: data
+    })
+
+  } catch (error) {
+    console.log(error);
+    dispatch(
+      {
+        type: orderConstants.ORDER_DETAILS_FAIL,
+        payload: error.response && error.response.data.message ? error.response.data.message : error.message
+      }
+    )
+  }
+}
+
+export const payOrder = (orderId, paymentResult) => async (
+  dispatch,
+  getState
+) => {
+  try {
+    dispatch({
+      type: orderConstants.ORDER_PAY_REQUEST,
+    })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    const { data } = await axios.put(
+      `/api/orders/${orderId}/pay`,
+      paymentResult,
+      config
+    )
+
+    dispatch({
+      type: orderConstants.ORDER_PAY_SUCCESS,
+      payload: data,
+    })
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message
+    dispatch({
+      type: orderConstants.ORDER_PAY_FAIL,
+      payload: message,
+    })
+  }
+}
