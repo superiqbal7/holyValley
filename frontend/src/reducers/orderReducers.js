94888dc67a280939c1ae94682116136eadeb6852
Rahim Iqbal <superiqu@gmail.com>
2021-04-09 01:36:11 +0600

    add order details & order pay reducer

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -19,3 +19,48 @@ export const orderCreateReducer = (state = {}, action) => {
       return state
   }
 }
+
+export const orderDetailsReducer = (state = { loading: true, orderItems: [], shippingAddress: {}}, action) => {
+  switch (action.type) {
+    case orderConstants.ORDER_DETAILS_REQUEST:
+      return { 
+        ...state,
+        loading: true 
+      }
+    case orderConstants.ORDER_DETAILS_SUCCESS:
+      return {
+        loading: false,
+        order: action.payload
+      }
+    case orderConstants.ORDER_DETAILS_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      }
+    default:
+      return state
+  }
+}
+
+export const orderPayReducer = (state = {}, action) => {
+  switch (action.type) {
+    case orderConstants.ORDER_PAY_REQUEST:
+      return {
+        loading: true
+      }
+    case orderConstants.ORDER_PAY_SUCCESS:
+      return {
+        loading: false,
+        success: true
+      }
+    case orderConstants.ORDER_PAY_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      }
+    case orderConstants.ORDER_PAY_RESET:
+      return {}
+    default:
+      return state
+  }
+}
