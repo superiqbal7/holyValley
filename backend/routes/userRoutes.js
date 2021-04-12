da2d8f396d5d7d449f96463dd427bdcd40227550
Rahim Iqbal <superiqu@gmail.com>
2021-04-13 01:35:55 +0600

    add get user route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,18 @@
 import express from 'express'
-import { authUser, registerUser, getUserProfile, updateUserProfile } from '../controller/userController.js'
-import { protect } from '../middleware/authMiddleware.js'
+import { 
+  authUser, 
+  registerUser, 
+  getUserProfile, 
+  updateUserProfile,
+  getUsers
+} from '../controller/userController.js'
+import { protect, isAdmin } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
-router.route('/').post(registerUser)
+router.route('/')
+  .post(registerUser)
+  .get(protect, isAdmin, getUsers)
 
 router.post('/login', authUser)
 
