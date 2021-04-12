24a94c8fdc42f9a49daef9750d16e78a671fb9b0
Rahim Iqbal <superiqu@gmail.com>
2021-04-13 01:34:36 +0600

    add userlist reducer

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -58,3 +58,19 @@ export const userUpdateProfileReducer = (state = {}, action) => {
       return state
   }
 }
+
+
+export const userListReducer = (state = { users: []}, action) => {
+  switch (action.type) {
+    case userConstants.USER_LIST_REQUEST:
+      return { loading: true }
+    case userConstants.USER_LIST_SUCCESS:
+      return { loading: false, users: action.payload }
+    case userConstants.USER_LIST_FAIL:
+      return { loading: false, error: action.payload }
+    case userConstants.USER_LIST_RESET:
+      return { users: []}
+    default:
+      return state
+  }
+}
