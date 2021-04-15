c62a52167bd3c30368a998fbdf1544e76c05bf40
Rahim Iqbal <superiqu@gmail.com>
2021-04-15 17:05:37 +0600

    add delete product action

--- frontend/src/actions/productAction.js
+++ frontend/src/actions/productAction.js
@@ -50,3 +50,43 @@ export const listProductDetails = (id) => async (dispatch) => {
     )
   }
 }
+
+
+export const deleteProduct = (id) => async (
+  dispatch,
+  getState
+) => {
+  try {
+    dispatch({
+      type: productConstants.PRODUCT_DELETE_REQUEST,
+    })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    await axios.delete(
+      `/api/products/${id}`,
+      config
+    )
+
+    dispatch({
+      type: productConstants.PRODUCT_DELETE_SUCCESS
+    })
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message
+    dispatch({
+      type: productConstants.PRODUCT_DELETE_FAIL,
+      payload: message,
+    })
+  }
+}
