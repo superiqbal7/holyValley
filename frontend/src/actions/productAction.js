12ba1e6ed13a239943a5d1d03b1482d68c98b971
Rahim Iqbal <superiqu@gmail.com>
2021-04-16 16:01:03 +0600

    add product create actions

--- frontend/src/actions/productAction.js
+++ frontend/src/actions/productAction.js
@@ -90,3 +90,41 @@ export const deleteProduct = (id) => async (
     })
   }
 }
+
+export const createProduct = () => async (
+  dispatch,
+  getState
+) => {
+  try {
+    dispatch({
+      type: productConstants.PRODUCT_CREATE_REQUEST,
+    })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    const { data } = await axios.post(
+      `/api/products`, {}, config)
+
+    dispatch({
+      type: productConstants.PRODUCT_CREATE_SUCCESS,
+      payload: data
+    })
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message
+    dispatch({
+      type: productConstants.PRODUCT_CREATE_FAIL,
+      payload: message,
+    })
+  }
+}
