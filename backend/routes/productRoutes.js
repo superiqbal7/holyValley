328f68f249e0f5d0e346de339689fc561b69a1a8
Rahim Iqbal <superiqu@gmail.com>
2021-04-16 16:01:57 +0600

    add create & update product route

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,16 +1,26 @@
 import express from 'express'
 import { protect, isAdmin } from '../middleware/authMiddleware.js'
-import { getProducts, getProductById, deleteProduct} from '../controller/productController.js'
+import { 
+  getProducts, 
+  getProductById, 
+  deleteProduct,
+  createProduct,
+  updateProduct
+} from '../controller/productController.js'
 
 const router = express.Router()
 
-router.route('/').get(getProducts)
+router
+  .route('/')
+  .get(getProducts)
+  .post(protect, isAdmin, createProduct)
 
 
 router
   .route('/:id')
   .get(getProductById)
   .delete(protect, isAdmin, deleteProduct)
+  .put(protect, isAdmin, updateProduct)
 
 
 export default router
