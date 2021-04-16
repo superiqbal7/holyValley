1980c908b1931c8a8d99cb33c91d191cfdfee047
Rahim Iqbal <superiqu@gmail.com>
2021-04-16 16:01:32 +0600

    add index.js

---
+++ frontend/src/screens/index.js
@@ -0,0 +1,31 @@
+import HomeScreen from './HomeScreen'
+import ProductScreen from './ProductScreen'
+import CartScreen from './CartScreen'
+import LoginScreen from './LoginScreen'
+import RegisterScreen from './RegisterScreen'
+import ProfileScreen from './ProfileScreen'
+import ShippingScreen from './ShippingScreen'
+import PaymentScreen from './PaymentScreen'
+import PlaceOrderScreen from './PlaceOrderScreen'
+import OrderScreen from './OrderScreen'
+import UserListScreen from './UserListScreen'
+import UserEditScreen from './UserEditScreen'
+import ProductListScreen from './ProductListScreen'
+import ProductEditScreen from './ProductEditScreen'
+
+export {
+  HomeScreen,
+  ProductScreen,
+  CartScreen,
+  LoginScreen,
+  RegisterScreen,
+  ProfileScreen,
+  ShippingScreen,
+  PaymentScreen,
+  PlaceOrderScreen,
+  OrderScreen,
+  UserListScreen,
+  UserEditScreen,
+  ProductEditScreen,
+  ProductListScreen
+}
