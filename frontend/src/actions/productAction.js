59bea808e360e6afd6943fc61c08b84b0390f95c
Rahim Iqbal <superiqu@gmail.com>
2021-04-17 13:47:52 +0600

    add product Update action

--- frontend/src/actions/productAction.js
+++ frontend/src/actions/productAction.js
@@ -128,3 +128,42 @@ export const createProduct = () => async (
     })
   }
 }
+
+export const updateProduct = (product) => async (
+  dispatch,
+  getState
+) => {
+  try {
+    dispatch({
+      type: productConstants.PRODUCT_UPDATE_REQUEST,
+    })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        'content-type': 'application/json',
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    const { data } = await axios.put(
+      `/api/products/${product._id}`, product, config)
+
+    dispatch({
+      type: productConstants.PRODUCT_UPDATE_SUCCESS,
+      payload: data
+    })
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message
+    dispatch({
+      type: productConstants.PRODUCT_UPDATE_FAIL,
+      payload: message,
+    })
+  }
+}
