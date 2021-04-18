2efd161ca3b9b76cfc49fb4c40f12d2127f985d8
Rahim Iqbal <superiqu@gmail.com>
2021-04-19 01:55:46 +0600

    add updateOrderToDelivered

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -4,7 +4,8 @@ import {
   getOrderById,
   updateOrderToPaid,
   getMyOrders,
-  getOrders
+  getOrders,
+  updateOrderToDelivered
 } from '../controller/orderController.js'
 import { protect, isAdmin } from '../middleware/authMiddleware.js'
 
@@ -20,6 +21,8 @@ router.route('/myorders').get(protect, getMyOrders)
 router.route('/:id').get(protect, getOrderById)
 
 router.route('/:id/pay').put(protect, updateOrderToPaid)
+
+router.route('/:id/deliver').put(protect, isAdmin, updateOrderToDelivered)
 
 
 export default router
