8fce0c8c0fb2c75afe073bc0e69aae725e75bc45
Rahim Iqbal <superiqu@gmail.com>
2021-04-19 01:54:29 +0600

    add order deliver

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -108,6 +108,44 @@ export const payOrder = (orderId, paymentResult) => async (
   }
 }
 
+export const deliverOrder = (order) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: orderConstants.ORDER_DELIVER_REQUEST,
+    })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    const { data } = await axios.put(
+      `/api/orders/${order._id}/deliver`,
+      {},
+      config
+    )
+
+    dispatch({
+      type: orderConstants.ORDER_DELIVER_SUCCESS,
+      payload: data,
+    })
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message
+    dispatch({
+      type: orderConstants.ORDER_DELIVER_FAIL,
+      payload: message,
+    })
+  }
+}
+
 export const listUserOrders = () => async (
   dispatch,
   getState
--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -65,6 +65,29 @@ export const orderPayReducer = (state = {}, action) => {
   }
 }
 
+export const orderDeliverReducer = (state = {}, action) => {
+  switch (action.type) {
+    case orderConstants.ORDER_DELIVER_REQUEST:
+      return {
+        loading: true
+      }
+    case orderConstants.ORDER_DELIVER_SUCCESS:
+      return {
+        loading: false,
+        success: true
+      }
+    case orderConstants.ORDER_DELIVER_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      }
+    case orderConstants.ORDER_DELIVER_RESET:
+      return {}
+    default:
+      return state
+  }
+}
+
 export const orderListUserReducer = (state = { orders: []}, action) => {
   switch (action.type) {
     case orderConstants.ORDER_LIST_USER_REQUEST:
@@ -110,3 +133,4 @@ export const orderListReducer = (state = { orders: [] }, action) => {
       return state
   }
 }
+
