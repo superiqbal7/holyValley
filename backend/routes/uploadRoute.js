cdb4e71131fb743064f0dbd674985e3151cf102d
Rahim Iqbal <superiqu@gmail.com>
2021-04-18 12:26:49 +0600

    fix

--- backend/routes/uploadRoute.js
+++ backend/routes/uploadRoute.js
@@ -1,24 +1,26 @@
+import path from 'path'
 import express from 'express'
 import multer from 'multer'
-import path from 'path'
-
 const router = express.Router()
 
 const storage = multer.diskStorage({
-  destination(req, file, cb){
+  destination(req, file, cb) {
     cb(null, 'uploads/')
   },
-  filename(req, file, cb){
-    cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`)
-  }
+  filename(req, file, cb) {
+    cb(
+      null,
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+    )
+  },
 })
 
-function checkFileType(file, cb){
-  const fileTypes = /jpg|jpeg|png/
-  const extname = fileTypes.test(path.extname(file.originalname).toLowerCase())
-  const mimetype = fileTypes.test(file.mimetype)
+function checkFileType(file, cb) {
+  const filetypes = /jpg|jpeg|png/
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase())
+  const mimetype = filetypes.test(file.mimetype)
 
-  if(extname && mimetype) {
+  if (extname && mimetype) {
     return cb(null, true)
   } else {
     cb('Images only!')
@@ -27,12 +29,12 @@ function checkFileType(file, cb){
 
 const upload = multer({
   storage,
-  fileFilter: function(req, file, cb){
+  fileFilter: function (req, file, cb) {
     checkFileType(file, cb)
-  }
+  },
 })
 
-router.post('/', upload.single('images'), (req, res) => {
+router.post('/', upload.single('image'), (req, res) => {
   res.send(`/${req.file.path}`)
 })
 
