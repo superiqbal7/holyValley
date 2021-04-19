69b19588d2737694ca7803c5c9ce058dc1fb2b03
Rahim Iqbal <superiqu@gmail.com>
2021-04-20 01:35:03 +0600

    add search by keyword

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -6,15 +6,17 @@ import { listProduct } from '../actions/productAction'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
 
-const HomeScreen = () => {
+const HomeScreen = ({ match }) => {
+  const keyword = match.params.keyword
+
   const dispatch = useDispatch()
 
   const productList = useSelector(state => state.productList)
   const { loading, error, products } = productList
 
   useEffect(() => {
-    dispatch(listProduct())
-  }, [dispatch])
+    dispatch(listProduct(keyword))
+  }, [dispatch, keyword])
 
   return (
     <>
