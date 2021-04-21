2fb4d8a42d3df75b3da9c4d60c3fb1241911a7e2
Rahim Iqbal <superiqu@gmail.com>
2021-04-21 16:28:50 +0600

    fix

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -23,7 +23,7 @@ const Product = ({ product }) => {
             text={`${product.numReviews} reviews`} 
           />
         </Card.Text>
-        <Card.Text as='h3'>${product.price}</Card.Text>
+        <Card.Text as='h3'>৳{product.price}</Card.Text>
       </Card.Body>
     </Card>
   )
