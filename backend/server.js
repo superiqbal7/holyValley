77a3afeffceb330e328d3996f02df79f19cf3502
Rahim Iqbal <superiqu@gmail.com>
2021-04-23 01:22:42 +0600

    setup heroku

--- backend/server.js
+++ backend/server.js
@@ -22,9 +22,7 @@ if(process.env.NODE_ENV === 'development'){
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Api is running')
-})
+
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
@@ -35,9 +33,25 @@ app.get('/api/config/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID)
 })
 
+
+
 //make folder static
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+ app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+ app.get('*', (req, res) => {
+   res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+ })
+
+} 
+else {
+  app.get('/', (req, res) => {
+    res.send('Api is running')
+  })
+}
 
 app.use(notFound)
 app.use(errorHandler) 
