e08b3920853958c51176dd260a22f905c8828b7c
Rahim Iqbal <superiqu@gmail.com>
2021-04-28 00:13:47 +0600

    add typescript

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -8,7 +8,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <Provider store={store}>
+  <Provider store={store: any}>
     <App />
   </Provider>,
   document.getElementById('root')
