7c41e88bfc7b18cbf3126e55bebc2521ba46dbe5
Rahim Iqbal <superiqu@gmail.com>
2021-05-20 01:32:23 +0600

    add infinitive scrolling

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { Row, Col } from 'react-bootstrap'
@@ -9,54 +9,86 @@ import Loader from '../components/Loader'
 import Paginate from '../components/Paginate'
 import ProductCarousel from '../components/ProductCarousel'
 import Meta from '../components/Meta'
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const HomeScreen = ({ match }) => {
   const keyword = match.params.keyword
 
-  const pageNumber = match.params.pageNumber || 1
+  //let pageNumber = match.params.pageNumber || 1
+  const [pageNumber, setPageNumber] = useState(match.params.pageNumber || 1)
 
   const dispatch = useDispatch()
 
   const productList = useSelector(state => state.productList)
   const { loading, error, products, page, pages } = productList
+  // if(products.length > 0) {
+  //   setPageNumber(page)
+  // }
+
+  //fetch products for infiniteScroll
+  const fetchMoreData = () => {
+    //setPageNumber(page)
+    if (page < pages){
+      setPageNumber(page + 1)
+    }
+  }
 
   useEffect(() => {
-    dispatch(listProduct(keyword, pageNumber))
-  }, [dispatch, keyword, pageNumber])
+    // if(products.length > 0){
+    //   setPageNumber(page)
+    // }
+    dispatch(listProduct(keyword, page ? page + 1 : pageNumber, products))
+  }, [dispatch, pageNumber])
 
   return (
-    <>
-      <Meta />
-      {!keyword ? (
-        <ProductCarousel />
-      ) : (
-        <Link to='/' className='btn btn-light'>
-          Go Back
-        </Link>
-      )}
-      <h1>Latest Products</h1>
-      {loading ? <Loader />
-        : error ? <Message variant='danger'>{error}</Message>
-          : (
-            <>
-              <Row>
-                {products.map((product) => (
-                  <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                    <Product product={product} />
-                  </Col>
-                ))}
-              </Row>
-              <Paginate
+    <div style={{overflow: 'hidden'}}>
+    
+        <Meta />
+        {!keyword ? (
+          <ProductCarousel />
+        ) : (
+          <Link to='/' className='btn btn-light'>
+            Go Back
+          </Link>
+        )}
+        <h1>Latest Products</h1>
+        {loading ? <Loader />
+          : error ? <Message variant='danger'>{error}</Message>
+            : products.length > 0 && (
+              <>
+                <InfiniteScroll
+                  dataLength={products.length}
+                  next={fetchMoreData}
+                  hasMore={page < pages ? true : true}
+                  loader={page < pages ? <Loader /> : ''}
+                  endMessage={
+                    <p style={{ textAlign: 'center' }}>
+                      <b>Yay! You have seen it all</b>
+                    </p>
+                  }
+                style={{ overflow: 'hidden' }}
+                >
+                  <Row>
+                    {products.map((product) => (
+                      <Col key={product._id} sm={6} md={3} lg={2} xl={2}>
+                        <Product product={product} />
+                      </Col>
+                    ))}
+                  </Row>
+                </InfiniteScroll>
+
+                {/* <Paginate
                 pages={pages}
                 page={page}
                 keyword={keyword ? keyword : ''}
-              />
-            </>
-          )
+              /> */}
+              </>
+            )
 
-      }
+        }
 
-    </>
+      
+    </div>
   )
 }
 
