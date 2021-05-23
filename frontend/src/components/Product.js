07b3e3d91b6a7d9e45e5be4a2e337dee46ef054a
Rahim Iqbal <superiqu@gmail.com>
2021-05-24 00:25:04 +0600

    truncate title

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -28,7 +28,7 @@ const Product = ({ product, history }) => {
       </Link>
       <Card.Body>
         <Link to={`/product/${product._id}`}>
-          <Card.Title as='div'>
+          <Card.Title as='div' className='crop-text-2'>
             <strong>{product.name}</strong>
           </Card.Title>
         </Link>
